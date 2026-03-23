b6734f0c01318292484bd977419ef24b0e6b8569
ihmaditaha <ihmaditaha1@gmail.com>
2026-03-23 20:11:34 +0100

    made custom requests for models

---
+++ app/Http/Requests/TaskRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:todo,in_progress,don',
+            'priority' => 'nullable|in:low,medium,high',
+            'deadline' => 'nullable|date',
+            'assigned_to' => 'nullable|exists:users,id',
+        ];
+    }
+}
