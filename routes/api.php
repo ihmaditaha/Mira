12cae3c1d6a0b376c56bbca94d409328537fde3e
ihmaditaha <ihmaditaha1@gmail.com>
2026-03-26 20:38:43 +0100

    changed routes and added the rest

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\WorkspaceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [Authcontroller::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+
+    Route::apiResource('workspaces', WorkspaceController::class);
+
+    Route::get('/workspaces/{workspace}/projects', [ProjectController::class, 'index']);
+    Route::post('/workspaces/{workspace}/projects', [ProjectController::class, 'store']);
+    Route::get('/projects/{project}', [ProjectController::class, 'show']);
+    Route::put('/projects/{project}', [ProjectController::class, 'update']);
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy']);
+
+    Route::get('/projects/{project}/tasks', [TaskController::class, 'index']);
+    Route::post('/projects/{project}/tasks', [TaskController::class, 'store']);
+    Route::get('/projects/{project}/tasks/{task}', [TaskController::class, 'show']);
+    Route::put('/projects/{project}/tasks/{task}', [TaskController::class, 'update']);
+    Route::delete('/projects/{project}/tasks/{task}', [TaskController::class, 'destroy']);
+});
